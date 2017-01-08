c7d50643901cfd81199c47087eefbf0ea4abfaa8
alexey-zhukovin <alexey.zhukovin@dsr-company.com>
2017-01-08 15:53:38 +0300

    debugging

--- src/forecast.py
+++ src/forecast.py
@@ -7,6 +7,23 @@ import xml.etree.ElementTree as ET
 import max7219.led as led
 from max7219.font import proportional, SINCLAIR_FONT, TINY_FONT, CP437_FONT
 
+def getYandexRootXML () :
+    try:
+        return downloadYandexFc()
+    except:
+        err = "Yandex Err:", sys.exc_info()[0]
+        print "download forecast failed"
+        return None
+
+def getGismeteo () :
+    try:
+        fc_text = download_fc()
+        return fc_text
+    except:
+        err = "Err:", sys.exc_info()[0]
+        print "download GISMETEO forecast failed"
+        return None
+
 def downloadYandexFc():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
@@ -57,35 +74,43 @@ device = led.matrix(cascaded=4, spi_bus=0, spi_device=0, vertical=True)
 
 while True:
     print "process new iteration"
-    try:
-        print "download forecast"
-        fc_text = download_fc()
-        root = ET.fromstring(fc_text)
-    except:
-        err = "Err:", sys.exc_info()[0]
-        print "download forecast failed"
-        print err
-        device.show_message(err, font=proportional(CP437_FONT))
+    yandex = getYandexRootXML()
+    gismeteo = getGismeteo()
+
+    currentYandex = None
+    if yandex != None:
+        try:
+            currentYandex = parseYandex ( yandex )
+        except:
+            print "yandex parse err"
+            print yandex
+
+    gismeteRootNode = None
+    if gismeteo != None:
+        try:
+            gismeteRootNode = ET.fromstring(gismeteo)
+        except:
+            print "parse gismete failed"
+
+    if gismeteRootNode is None and currentYandex is None:
+        device.brightness(1)
+        device.show_message("Error getting data ...", font=proportional(CP437_FONT))
         time.sleep(10)
         continue
 
-    try:
-        yandexData = downloadYandexFc()
-        currentYandex = parseYandex ( yandexData )
-    except:
-        currentYandex = None
-        err = "Err:", sys.exc_info()[0]
-        print "download yandex forecast failed"
-        print err
+    fc0 = ""
+    fc1 = ""
+    fc2 = ""
 
+    if gismeteRootNode is not None:
+        fc0 = formatForecastGismeteo(gismeteRootNode[0][0][0])
+        fc0 = "gismeteo %s" % (fc0)
+        fc1 = '>%s' % formatForecastGismeteo(gismeteRootNode[0][0][1])
+        fc2 = '>>%s' % formatForecastGismeteo(gismeteRootNode[0][0][2])
 
-    fc0 = formatForecastGismeteo(root[0][0][0])
-    if currentYandex != None:    
+    if currentYandex != None:
         fcYa = formatForecastYandex(currentYandex)
-        fc0 = "yandex %s gismeteo %s" % (fcYa, fc0)
-
-    fc1 = '>%s' % formatForecastGismeteo(root[0][0][1])
-    fc2 = '>>%s' % formatForecastGismeteo(root[0][0][2])
+        fc0 = "yandex %s %s" % (fcYa, fc0)
 
     for x in range(100):
         device.brightness(15)
